5e7fa870d3f1579500d8a22b829ec3d9402b8039
Enrique <gcpamdocs@gmail.com>
2019-06-18 22:35:12 -0500

    Project Directory

---
+++ directorio.py
@@ -0,0 +1,11 @@
+
+
+print ("Directorio de Miembros de la familia")
+
+print ("1.Listado de Miembros de la familia")
+print ("2.Agregar nuevo integrante")
+print ("3.Busqueda de integrante")
+print ("4.Salir")
+print ("Elija una opcion del Menú.... presione 4 para salir")
+strOpcion = input()
+print ("Haz Elegido" , strOpcion)
